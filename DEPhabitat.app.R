040bc21019f0d50be87a8735f2faaf4343ea0381
bwcompton <bcompton@umass.edu>
2023-07-27 12:48:33 -0400

    Add ?maptype=2 option to serve 5 color IEI map!

--- DEPhabitat.app.R
+++ DEPhabitat.app.R
@@ -1,5 +1,6 @@
 # DEPhabitat app
-# MassDEP Habitat of Potential Regional or Statewide Importance viewer Shiny app
+# MassDEP Habitat of Potential Regional or Statewide Importance/five color IEI viewer Shiny app
+# for embedding in umasscaps.org
 # See https://github.com/bwcompton/DEPhabitat
 # Before initial deployment on shinyapps.io, need to restart R and:
 #    library(remotes); install_github('bwcompton/readMVT'); install_github('bwcompton/leaflet.lagniappe')
@@ -17,13 +18,20 @@ zoom <- 8                     # starting zoom level (shows all of Massachusetts)
 
 ui <- fluidPage(
    tags$head(includeScript('inst/matomo.js')),              # add Matomo tracking JS
+
    mainPanel(
       leafletOutput('map', width = '680')
    )
 )
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+   observe({                  # get maptype parameter from URL
+      session$userData$maptype <- as.numeric(parseQueryString(session$clientData$url_search)$maptype)[1]
+      if(is.na(session$userData$maptype)) session$userData$maptype <- 1  # default is important habitat map
+   })
+
    output$map <- renderLeaflet({
       leaflet() |>
          addTiles(urlTemplate = '', attribution =
@@ -34,9 +42,8 @@ server <- function(input, output) {
          addProviderTiles(providers$Esri.WorldTopoMap) |>
          addWMSTiles(
             'https://umassdsl.webgis1.com/geoserver/wms',
-            layers = 'DEPhabitat:DEPhabitat',
-            options = WMSTileOptions(opacity = 0.25),
-         ) |>
+            layers = switch(session$userData$maptype , 'DEPhabitat:DEPhabitat', 'DEPhabitat:fivecolor'),
+            options = WMSTileOptions(opacity = switch(session$userData$maptype , 0.25, 0.4))) |>
          setView(lng = home[1], lat = home[2], zoom = zoom) |>
          osmGeocoder(email = 'bcompton@umass.edu') |>
          addScaleBar(position = 'bottomright') |>
